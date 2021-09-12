2907c77e0f9fab0f85d11f17ed906d8bc198f4ff
yhrazhevskyi <yhrazhevskyi@gmail.com>
2021-09-12 21:28:34 +0300

    little improvements

--- time.py
+++ time.py
@@ -1,5 +1,5 @@
 total_second = float(input("Введите кол-во сек. : "))
-hours = int(total_second // 3600)
-minutes = int((total_second // 60) % 60)
-seconds = int(total_second % 60)
-print(f'time is {hours}:{minutes}:{seconds}')
+hours = total_second // 3600
+minutes = total_second % 3600 // 60
+seconds = total_second % 3600 % 60
+print(f'time is {int(hours)}:{int(minutes)}:{int(seconds)}')
